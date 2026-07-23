5917cfc490a46963c8bfc457d5c4694d6aa2b6f6
Aditya Jain <aj149413@gmail.com>
2026-07-23 20:06:55 +0530

    Add implementations for Dubstep and KefaandFirstSteps solutions

---
+++ Dubstep.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Dubstep {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arr = str.split("WUB");
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            if (!s.isEmpty()) {
+                sb.append(s).append(" ");
+            }
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
