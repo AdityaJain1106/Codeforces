1353ddfb13b78bc9e481799e4a8f36ba91276471
Aditya Jain <aj149413@gmail.com>
2026-07-28 20:02:16 +0530

    Add implementations for StringTask, TheatreSquare, and YoungPhysicist solutions

---
+++ StringTask.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class StringTask {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        StringBuilder ans = new StringBuilder();
+
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch != 'a' && ch != 'o' && ch != 'y' &&
+                ch != 'e' && ch != 'u' && ch != 'i') {
+                ans.append('.');
+                ans.append(ch);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
---
+++ TheatreSquare.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TheatreSquare{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long m = scanner.nextLong();
+        long a = scanner.nextLong();
+        long result = (long) Math.ceil((double) n / a) * (long) Math.ceil((double) m / a);
+        System.out.println(result);
+    }
+}
