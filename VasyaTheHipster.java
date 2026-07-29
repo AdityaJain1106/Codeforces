46b38fd8f35d2569b60de62d5f96ecee329f88b6
Aditya Jain <aj149413@gmail.com>
2026-07-29 18:26:34 +0530

    Add implementations for Elephant and VasyaTheHipster solutions

---
+++ VasyaTheHipster.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class VasyaTheHipster {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a = Math.min(n, m);
+        int b = Math.max(n, m);
+        int ans1 = a;
+        int ans2 = (b - a) / 2;
+        System.out.println(ans1 + " " + ans2);
+    }
+    
+}
