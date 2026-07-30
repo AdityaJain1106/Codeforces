a27c5bc769b232a404fed280488577b82c1e8d6c
Aditya Jain <aj149413@gmail.com>
2026-07-31 01:10:25 +0530

    Add implementations for ChatRoom, Expression, LuckyDivision, and cAPSIOCK solutions

---
+++ Expression.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Expression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        System.out.println(Math.max(n1+n2+n3, Math.max(n1*n2*n3, Math.max(n1*(n2+n3), Math.max((n1+n2)*n3, Math.max(n1*n2+n3, n1+n2*n3))))));
+    }
+    
+}
---
+++ cAPSIOCK.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class cAPSIOCK {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        boolean ok = true;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (Character.isLowerCase(s.charAt(i))) {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok) {
+            StringBuilder ans = new StringBuilder();
+
+            for (char ch : s.toCharArray()) {
+                if (Character.isUpperCase(ch))
+                    ans.append(Character.toLowerCase(ch));
+                else
+                    ans.append(Character.toUpperCase(ch));
+            }
+
+            System.out.println(ans);
+        } else {
+            System.out.println(s);
+        }
+    }
+}
