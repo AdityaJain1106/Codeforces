dac010034cd825bafbaf8803c6bf2f3c76496a6f
Aditya Jain <aj149413@gmail.com>
2026-08-01 21:30:00 +0530

    Add implementations for Dragons and XeniaAndRingroad solutions

---
+++ XeniaAndRingroad.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class XeniaAndRingroad {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int noOfHouses= sc.nextInt();
+        int noOfTask= sc.nextInt();
+
+        long time = 0;
+        int current = 1;
+
+        while (noOfTask-- > 0) {
+            int next = sc.nextInt();
+
+            if (next >= current)
+                time += next - current;
+            else
+                time += noOfHouses- current + next;
+
+            current = next;
+        }
+
+        System.out.println(time);
+    }
+}
